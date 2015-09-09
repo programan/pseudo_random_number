9a333e26f4148263cbf071fe99c58c649ee268ef
programan <github@programan.com>
2015-09-09 22:13:34 +0900

    線形合同法で乱数生成
    * 周期をいろいろ
    * 範囲指定

--- sandbox1/python/linear_congruential_generator.py
+++ sandbox1/python/linear_congruential_generator.py
@@ -21,7 +21,7 @@ RAND_MAX = 0x7fff
 rnd_next = 1
 
 
-def test_linear_rnd1(max):
+def test_linear_rnd1(maximum):
     """
     乱数生成
     """
@@ -36,7 +36,7 @@ def test_linear_rnd1(max):
     return int(rnd_next)
 
 
-def test_linear_rnd2(max):
+def test_linear_rnd2(maximum):
     """
     乱数生成
     """
@@ -53,7 +53,7 @@ def test_linear_rnd2(max):
     return int(rnd_next & 0x7)
 
 
-def test_linear_rnd3(max):
+def test_linear_rnd3(maximum):
     """
     乱数生成
     """
@@ -68,6 +68,73 @@ def test_linear_rnd3(max):
     # 周期が少くとも536870912になる(のかな)
     # 536870912個の乱数を出力すると同じパターン戻る
     return int(rnd_next >> 29 & 0x7)
+
+
+def test_linear_rnd4(maximum):
+    """
+    乱数生成
+    0から引数の値未満の乱数を返す
+    引数が0なら2**32とする
+    """
+
+    if maximum <= 0:
+        maximum = 2 ** 32
+
+    # gccと同じ組み合わせで生成
+    global rnd_next
+    # VCとかと同じように32bitに収まるようにする
+    # value % 2^32 == value & 2^32-1
+    rnd_next = (rnd_next * 1103515245 + 12345) & 0xffffffff
+
+    # 上位8ビットのみを使ってみる
+    # 0からmaximum未満で乱数を生成
+    return int(rnd_next >> 24 & 0xff) % maximum
+
+
+def test_linear_rnd5(maximum):
+    """
+    乱数生成
+    0から引数の値未満の乱数を返す
+    引数が0なら2**32とする
+    """
+
+    if maximum <= 0:
+        maximum = 2 ** 32
+
+    # gccと同じ組み合わせで生成
+    global rnd_next
+    # VCとかと同じように32bitに収まるようにする
+    # value % 2^32 == value & 2^32-1
+    rnd_next = (rnd_next * 1103515245 + 12345) & 0xffffffff
+
+    # VCっぽく
+    # 上位15ビットのみを使ってみる
+    # 下位ビットを捨てるので周期が長くなるが、15ビットの値のみなので精度は落る
+    # 0からmaximum未満で乱数を生成
+    return int(rnd_next >> 16 & RAND_MAX) % maximum
+
+
+def test_linear_rnd6(minimum, maximum):
+    """
+    乱数生成
+    0から引数の値未満の乱数を返す
+    引数が0なら2**32とする
+    """
+
+    if maximum <= 0:
+        maximum = 2 ** 32
+
+    # gccと同じ組み合わせで生成
+    global rnd_next
+    # VCとかと同じように32bitに収まるようにする
+    # value % 2^32 == value & 2^32-1
+    rnd_next = (rnd_next * 1103515245 + 12345) & 0xffffffff
+
+    # VCっぽく
+    # 上位15ビットのみを使ってみる
+    # 下位ビットを捨てるので周期が長くなるが、15ビットの値のみなので精度は落る
+    # minimumからmaximum未満で乱数を生成
+    return int(rnd_next >> 16 & RAND_MAX) % ((maximum + 1) - minimum) + minimum
 
 
 if __name__ == '__main__':
@@ -91,6 +158,30 @@ if __name__ == '__main__':
     print('test---------')
     for i in range(20):
         ret = test_linear_rnd3(128)
+        # print("%0b" % (ret))
+        value = "%d" % (ret)
+        print(str(value))
+        print("%s" % (bin(ret)))
+
+    print('test---------')
+    for i in range(20):
+        ret = test_linear_rnd4(128)
+        # print("%0b" % (ret))
+        value = "%d" % (ret)
+        print(str(value))
+        print("%s" % (bin(ret)))
+
+    print('test---------')
+    for i in range(20):
+        ret = test_linear_rnd5(128)
+        # print("%0b" % (ret))
+        value = "%d" % (ret)
+        print(str(value))
+        print("%s" % (bin(ret)))
+
+    print('test---------')
+    for i in range(20):
+        ret = test_linear_rnd6(20, 128)
         # print("%0b" % (ret))
         value = "%d" % (ret)
         print(str(value))
