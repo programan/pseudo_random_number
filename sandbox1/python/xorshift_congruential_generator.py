205fa93b4b6a5cd884a691ea17d400d4de786d00
programan <github@programan.com>
2015-09-17 20:56:48 +0900

    pythonのxorshiftを修正
    * maximumの値未満の方がいいのか、その値までの範囲が良いのか悩む

--- sandbox1/python/xorshift_congruential_generator.py
+++ sandbox1/python/xorshift_congruential_generator.py
@@ -1,79 +1,83 @@
 # -*- coding: utf-8 -*-
 
-# from decimal import *
-
 # xorshift
 # # paper : http://www.jstatsoft.org/v08/i14/
 # # 符号なしの32ビット整数を4つ使う
 # # 4つの変数をシフトしてテンポラリに代入し、他の変数同士ずらして代入してと、
 #   ぐるぐる回していくような感じで疑似乱数を生成する
 # # seedのx,y,z,wは0でない整数ならば何でも良い
-# # 周期は2^128 - 1となる
+# # 周期は2 ** 128 - 1となる
+# # 乱数の最大値は 2 ** 32で収まる値
+
+# この数に収まる乱数になる
+RAND_MAX = 2 ** 32
 
 # seed
+# まとめて代入
+# 各値は符号なし32bit整数に収まる範囲にする
 x, y, z, w = (123456789, 362436069, 521288629, 88675123)
 
 
 def seed(s):
+    """
+    乱数のseedを設定
+    seed([123456789, 861932, 5671, 232123])
+    """
     global x, y, z, w
     x, y, z, w = s
+    x &= RAND_MAX - 1
+    y &= RAND_MAX - 1
+    z &= RAND_MAX - 1
+    w &= RAND_MAX - 1
+
     if x + y + z + w <= 0:
-        return False
-        # raise ValueError, "Please do not substitute 0 for seed."
-    return True
+        raise ValueError("Please do not substitute 0 for seed.")
 
 
-def test_xorshift_rnd1(minimum, maximum):
+def test_xorshift_rnd1():
     global x, y, z, w
+    # Cとかみたいに変数にサイズとかある訳ではないので、
+    # これだと32bitをオーバーする場合がある
     t = (x ^ (x << 11))
     x, y, z = y, z, w
     w = (w ^ (w >> 19)) ^ (t ^ (t >> 8))
     return w
-    # return (minimum + w) % maximum
 
 
-def test_xorshift_rnd2(minimum, maximum):
+def test_xorshift_rnd2():
     global x, y, z, w
     # 32bit以内になるようにする
     # x % 4294967296
-    t = (x ^ (x << 11)) & 0xffffffff
+    t = (x ^ (x << 11)) & (RAND_MAX - 1)
     x, y, z = y, z, w
     w = (w ^ (w >> 19)) ^ (t ^ (t >> 8))
     return w
-    # return (minimum + w) % maximum
 
 
-def test_xorshift_rnd3(minimum, maximum):
+def test_xorshift_rnd3():
     global x, y, z, w
-    t = (x ^ (x << 11 & 0xffffffff))
-    x, y, z = y, z, w
-    w = (w ^ (w >> 19 & 0x1fff)) ^ (t ^ (t >> 8 & 0xffffff))
     # 32bit以内になるようにする
     # x % 4294967296
-    # return w & 0xffffffff
+    t = (x ^ (x << 11)) & (RAND_MAX - 1)
+    x, y, z = y, z, w
+    # pythonには符号なしとか無いので、右シフトした場合にシフトした分だけ、
+    # 左に0があるようにした値とマスクする。
+    # でも、この処理いらないかも
+    w = (w ^ (w >> 19 & 0x1fff)) ^ (t ^ (t >> 8 & 0xffffff))
     return w
-    # return (minimum + w) % maximum
 
 
 def test_xorshift_rnd4(minimum, maximum):
-    global x, y, z, w
-    # 32bit以内になるようにする
-    # x % 4294967296
-    t = (x ^ (x << 11)) & 0xffffffff
-    x, y, z = y, z, w
-    w = (w ^ (w >> 19)) ^ (t ^ (t >> 8))
-    return w
-    # return (minimum + w) % maximum
-
-
-def test_xorshift_rnd5(minimum, maximum):
+    """
+    最小値と最大値を指定
+    """
     global x, y, z, w
 
-    if minimum < 0:
+    if minimum < 0 or minimum > RAND_MAX:
         minimum = 0
 
-    if maximum <= 0:
-        maximum = 2 ** 32
+    if maximum <= 0 or maximum > RAND_MAX:
+        maximum = RAND_MAX
 
     if minimum > maximum:
         tmp = minimum
@@ -82,9 +86,10 @@ def test_xorshift_rnd5(minimum, maximum):
 
     # 32bit以内になるようにする
     # x % 4294967296
-    t = (x ^ (x << 11)) & 0xffffffff
+    t = (x ^ (x << 11)) & (RAND_MAX - 1)
     x, y, z = y, z, w
     w = (w ^ (w >> 19)) ^ (t ^ (t >> 8))
+    # 最小値から最大値未満にする
     return (minimum + w) % maximum
 
 
@@ -97,21 +102,21 @@ def test_xor_rnd_generator():
 
     # 32bit以内になるようにする
     # x % 4294967296
-    t = (x ^ (x << 11)) & 0xffffffff
+    t = (x ^ (x << 11)) & (RAND_MAX - 1)
     x, y, z = y, z, w
     w = (w ^ (w >> 19)) ^ (t ^ (t >> 8))
 
     return w
 
 
-def test_xorshift_rnd6(minimum, maximum):
+def test_xorshift_rnd5(minimum, maximum):
     global x, y, z, w
 
-    if minimum < 0:
+    if minimum < 0 or minimum > RAND_MAX:
         minimum = 0
 
-    if maximum <= 0:
-        maximum = 2 ** 32
+    if maximum <= 0 or maximum >= RAND_MAX:
+        maximum = (RAND_MAX - 1)
 
     if minimum > maximum:
         tmp = minimum
@@ -122,10 +127,8 @@ def test_xorshift_rnd6(minimum, maximum):
     return int((test_xor_rnd_generator() % ((maximum + 1) - minimum)) + minimum)
 
 
-def test_xorshift_rnd7():
+def test_xorshift_rnd6():
     global x, y, z, w
-
-    RAND_MAX = 2 ** 32
 
     # 0.0から1.0未満で乱数を生成
     # round()などで丸めこみはしない
@@ -138,7 +141,7 @@ if __name__ == '__main__':
     print('test---------')
     x, y, z, w = (123456789, 362436069, 521288629, 88675123)
     for i in range(20):
-        ret = test_xorshift_rnd1(1, 10)
+        ret = test_xorshift_rnd1()
         # print("%0b" % (ret))
         value = "%d" % (ret)
         print(str(value))
@@ -147,7 +150,7 @@ if __name__ == '__main__':
     print('test---------')
     x, y, z, w = (123456789, 362436069, 521288629, 88675123)
     for i in range(20):
-        ret = test_xorshift_rnd2(1, 10)
+        ret = test_xorshift_rnd2()
         # print("%0b" % (ret))
         value = "%d" % (ret)
         print(str(value))
@@ -156,7 +159,7 @@ if __name__ == '__main__':
     print('test---------')
     x, y, z, w = (123456789, 362436069, 521288629, 88675123)
     for i in range(20):
-        ret = test_xorshift_rnd3(1, 10)
+        ret = test_xorshift_rnd3()
         # print("%0b" % (ret))
         value = "%d" % (ret)
         print(str(value))
@@ -165,16 +168,17 @@ if __name__ == '__main__':
     print('test---------')
     x, y, z, w = (123456789, 362436069, 521288629, 88675123)
     for i in range(20):
-        ret = test_xorshift_rnd4(1, 10)
+        ret = test_xorshift_rnd4(1, 5)
         # print("%0b" % (ret))
-        value = "%010d" % (ret)
+        # value = "%010d" % (ret)
+        value = "%d" % (ret)
         print(str(value))
         # print("%s" % (bin(ret)))
 
     print('test---------')
     x, y, z, w = (123456789, 362436069, 521288629, 88675123)
-    for i in range(60):
-        ret = test_xorshift_rnd5(1, 64)
+    for i in range(20):
+        ret = test_xorshift_rnd5(1, 5)
         # print("%0b" % (ret))
         value = "%d" % (ret)
         print(str(value))
@@ -182,17 +186,8 @@ if __name__ == '__main__':
 
     print('test---------')
     x, y, z, w = (123456789, 362436069, 521288629, 88675123)
-    for i in range(60):
-        ret = test_xorshift_rnd6(1, 64)
-        # print("%0b" % (ret))
-        value = "%d" % (ret)
-        print(str(value))
-        # print("%s" % (bin(ret)))
-
-    print('test---------')
-    x, y, z, w = (123456789, 362436069, 521288629, 88675123)
-    for i in range(60):
-        ret = test_xorshift_rnd7()
+    for i in range(20):
+        ret = test_xorshift_rnd6()
         # print("%0b" % (ret))
         # value = "%f" % (ret)
         value = "%.16f" % (ret)
