2e7537dc4053dbf49226dce52db88213508bb441
programan <github@programan.com>
2016-08-16 16:12:31 +0900

    RubyのLinearを修正
    * XからYまでの範囲と指定する関数について、一部ルールがぶれていたのを修正
       * maximumを含む値までの乱数
    * XからYまでの範囲を求める式を修正したものを追加

--- sandbox1/ruby/linear_congruential_generator.rb
+++ sandbox1/ruby/linear_congruential_generator.rb
@@ -68,14 +68,14 @@ end
 
 def test_linear_rnd4(maximum)
   # 乱数生成
-  # 0から引数の値未満の乱数を返す
-  # 引数が0なら2**32とする
+  # 0から引数の値までの乱数を返す
+  # 引数が0ならRAND_MAXまでとする
 
-  # maximum = 2 ** 32 if maximum <= 0
   if maximum <= 0 or maximum > RAND_MAX
     maximum = RAND_MAX
   end
 
+  # 引数の値+1として、0から引数の値までの範囲で乱数を求める
   maximum += 1
 
   # gccと同じ組み合わせで生成
@@ -92,9 +92,14 @@ end
 def test_linear_rnd5(maximum)
   # 乱数生成
   # 0から引数の値未満の乱数を返す
-  # 引数が0なら2**32とする
+  # 引数が0ならRAND_MAXまでとする
 
-  maximum = 2 ** 32 if maximum <= 0
+  if maximum <= 0 or maximum > RAND_MAX
+    maximum = RAND_MAX
+  end
+
+  # 引数の値+1として、0から引数の値までの範囲で乱数を求める
+  maximum += 1
 
   # gccと同じ組み合わせで生成
   # VCとかと同じように32bitに収まるようにする
@@ -111,12 +116,18 @@ end
 
 def test_linear_rnd6(minimum, maximum)
   # 乱数生成
-  # 0から引数の値未満の乱数を返す
-  # 引数が0なら2**32とする
+  # 引数が0ならRAND_MAXまでとする
 
-  minimum = 0 if minimum < 0
+  if minimum < 0 or minimum > RAND_MAX
+    minimum = 0
+  end
 
-  maximum = 2 ** 32 if maximum <= 0
+  if maximum <= 0 or maximum > RAND_MAX
+    maximum = RAND_MAX
+  end
+
+  # 引数の値+1として、0から引数の値までの範囲で乱数を求める
+  maximum += 1
 
   if minimum > maximum
     tmp = minimum
@@ -133,7 +144,7 @@ def test_linear_rnd6(minimum, maximum)
   # 上位15ビットのみを使ってみる
   # 下位ビットを捨てるので周期が長くなるが、15ビットの値のみなので精度は落る
   # minimumからmaximum未満で乱数を生成
-  return (($rnd_next >> 16 & RAND_MAX) % ((maximum + 1) - minimum)) + minimum
+  return (($rnd_next >> 16 & RAND_MAX) % (maximum - minimum)) + minimum
 end
 
 
@@ -155,11 +166,18 @@ end
 def test_linear_rnd7(minimum, maximum)
   # 乱数生成
   # 0から引数の値未満の乱数を返す
-  # 引数が0なら2**32とする
+  # 引数が0ならRAND_MAXまでとする
 
-  minimum = 0 if minimum < 0
+  if minimum < 0 or minimum > RAND_MAX
+    minimum = 0
+  end
 
-  maximum = 2 ** 32 if maximum <= 0
+  if maximum <= 0 or maximum > RAND_MAX
+    maximum = RAND_MAX
+  end
+
+  # 引数の値+1として、0から引数の値までの範囲で乱数を求める
+  maximum += 1
 
   if minimum > maximum
     tmp = minimum
@@ -168,7 +186,7 @@ def test_linear_rnd7(minimum, maximum)
   end
 
   # minimumからmaximum未満で乱数を生成
-  return ((test_linear_rnd_generator() % ((maximum + 1) - minimum)) + minimum)
+  return ((test_linear_rnd_generator() % (maximum  - minimum)) + minimum)
 end
 
 
@@ -188,9 +206,37 @@ def test_linear_rnd8()
 end
 
 
+def test_linear_rnd9(minimum, maximum)
+  # 乱数生成
+  # 0から引数の値未満の乱数を返す
+  # 引数が0ならRAND_MAXまでとする
+
+  if minimum < 0 or minimum > RAND_MAX
+    minimum = 0
+  end
+
+  if maximum <= 0 or maximum > RAND_MAX
+    maximum = RAND_MAX
+  end
+
+  # 引数の値+1として、0から引数の値までの範囲で乱数を求める
+  maximum += 1
+
+  if minimum > maximum
+    tmp = minimum
+    minimum = maximum
+    maximum = tmp
+  end
+
+  # minimumからmaximum未満で乱数を生成
+  # 範囲の公式使用版(もちろんrnd7とは違う結果になる)
+  return minimum + ((test_linear_rnd_generator() * (maximum - minimum + 1.0) / (1.0 + RAND_MAX))).to_i
+end
+
+
 if __FILE__ == $0
 
-  puts('test---------')
+  puts('test1---------')
   $rnd_next = 1
   for i in 0...20
     ret = test_linear_rnd1(128)
@@ -199,7 +245,7 @@ if __FILE__ == $0
     puts("%#b" % ret)
   end
 
-  puts('test---------')
+  puts('test2---------')
   $rnd_next = 1
   for i in 0...20
     ret = test_linear_rnd2(128)
@@ -208,7 +254,7 @@ if __FILE__ == $0
     puts("%#b" % ret)
   end
 
-  puts('test---------')
+  puts('test3---------')
   $rnd_next = 1
   for i in 0...20
     ret = test_linear_rnd3(128)
@@ -217,7 +263,7 @@ if __FILE__ == $0
     puts("%#b" % ret)
   end
 
-  puts('test---------')
+  puts('test4---------')
   $rnd_next = 1
   for i in 0...20
     ret = test_linear_rnd4(128)
@@ -226,7 +272,7 @@ if __FILE__ == $0
     puts("%#b" % ret)
   end
 
-  puts('test---------')
+  puts('test5---------')
   $rnd_next = 1
   for i in 0...20
     ret = test_linear_rnd5(128)
@@ -235,7 +281,7 @@ if __FILE__ == $0
     puts("%#b" % ret)
   end
 
-  puts('test---------')
+  puts('test6---------')
   $rnd_next = 1
   for i in 0...20
     ret = test_linear_rnd6(20, 128)
@@ -244,7 +290,7 @@ if __FILE__ == $0
     puts("%#b" % ret)
   end
 
-  puts('test---------')
+  puts('test7---------')
   $rnd_next = 1
   for i in 0...20
     ret = test_linear_rnd7(20, 128)
@@ -253,12 +299,20 @@ if __FILE__ == $0
     puts("%#b" % ret)
   end
 
-  puts('test---------')
+  puts('test8---------')
   $rnd_next = 1
   for i in 0...40
     ret = test_linear_rnd8()
     puts("%.16f" % ret)
   end
 
+  puts('test9---------')
+  $rnd_next = 1
+  for i in 0...20
+    ret = test_linear_rnd9(20, 128)
+    puts(ret)
+    # puts(ret.to_s(2))
+    puts("%#b" % ret)
+  end
 end
 
